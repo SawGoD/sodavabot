a5d629d1c473b68101d3d0af5adbd2805186abbe
SawGoD <seegod13@gmail.com>
2023-06-23 02:01:55 +0300

    launch changes: undo

--- soda_va_bot_start.bat
+++ soda_va_bot_start.bat
@@ -4,10 +4,7 @@ taskkill /F /IM ShareX.exe
 taskkill /F /IM speedtest.exe
 
 cd C:\Soda_VA_BOT
-echo git pull > initial_launch.ps1
-powershell.exe -ExecutionPolicy Bypass -File ".\initial_launch.ps1"
 start /min "Soda VA Bot Monitor" python .\soda_va_bot.py 2> .\logs\error_bat.txt
 start .\resource\RBTray\64bit\RBTray.exe
 start .\resource\ShareX\ShareX.exe
-del initial_launch.ps1
 echo pause
