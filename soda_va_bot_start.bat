cc1e649ec78c5beccbf6a161606127162d65fb1d
SawGoD <seegod13@gmail.com>
2023-06-23 01:33:35 +0300

    launch changes: added git pull

--- soda_va_bot_start.bat
+++ soda_va_bot_start.bat
@@ -1,8 +1,11 @@
 @echo off
 taskkill /F /IM RBTray.exe
 taskkill /F /IM ShareX.exe
+taskkill /F /IM speedtest.exe
 
 cd C:\Soda_VA_BOT
+echo git pull > initial_launch.ps1
+powershell.exe -ExecutionPolicy Bypass -File ".\initial_launch.ps1"
 start /min "Soda VA Bot Monitor" python .\soda_va_bot.py 2> .\logs\error_bat.txt
 start .\resource\RBTray\64bit\RBTray.exe
 start .\resource\ShareX\ShareX.exe
