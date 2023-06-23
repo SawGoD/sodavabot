eaf4fdc1457029f77dbc7fb0b96ffdf75d85a26a
Никита Михайлович Кареликов <karelikov@MK7.corp>
2023-06-23 15:22:59 +0300

    -shareX +autopull

--- soda_va_bot_start.bat
+++ soda_va_bot_start.bat
@@ -1,9 +1,9 @@
 @echo off
 taskkill /F /IM RBTray.exe
-taskkill /F /IM ShareX.exe
 taskkill /F /IM speedtest.exe
 
 cd C:\Soda_VA_BOT
+git pull
 start /min "Soda VA Bot Monitor" python .\soda_va_bot.py 2> .\logs\error_bat.txt
 start .\resource\RBTray\64bit\RBTray.exe
 start .\resource\ShareX\ShareX.exe
