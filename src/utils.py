2e2bb9b230e9d455714659064c13cb02949c610d
SawGoD <flay.my.number.43@mail.ru>
2024-10-27 01:04:05 +0300

    feat: new func for summary buttons

--- src/utils.py
+++ src/utils.py
@@ -27,3 +27,32 @@ def read_json(*cells: str, filename: str = "titles_dict") -> Any:
         for cell in cells:
             result = result[cell]
         return result
+
+
+def write_json(value: Any, *cells: str, filename: str = "titles_dict") -> None:
+    with open(f"./data/{filename}.json", "r+", encoding="utf-8") as f:
+        data = json.load(f)
+        result = data
+        for cell in cells[:-1]:
+            result = result[cell]
+        result[cells[-1]] = value
+        f.seek(0)
+        json.dump(data, f, ensure_ascii=False, indent=2)
+        f.truncate()
+
+
+async def get_new_state_json(cells: list, filename: str = "fleeting_data"):
+    state = read_json(*cells, filename=filename)
+    new_state = (1 - state if isinstance(state, int) else state)  # Проверка, является ли state целым числом
+    write_json(new_state, *cells, filename=filename)
+    print(f"New state of {cells}: {new_state}")
+
+
+async def set_to_default_state():
+    values = (
+        "volume_expanded",
+        "output_expanded",
+    )
+    for value in values:
+        write_json(0, "summary_state", value, filename="fleeting_data")
+    print("Set to default state")
